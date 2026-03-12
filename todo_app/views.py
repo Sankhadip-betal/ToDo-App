249de18aaf8528153048a6919ca2dcc163ca9530
Sankhadip-betal <betalsankhdip6@gmail.com>
2026-03-12 20:35:48 +0530

    Initial commit

---
+++ todo_app/views.py
@@ -0,0 +1,119 @@
+
+# todo_list/todo_app/views.py
+from .models import ToDoItem, ToDoList
+from django.urls import reverse
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from django.views import View
+from django.shortcuts import redirect,get_object_or_404
+
+
+
+class ListListView(ListView):
+    model = ToDoList
+    template_name = "todo_app/index.html"
+
+class ItemListView(ListView):
+    model = ToDoItem
+    template_name = "todo_app/todo_list.html"
+
+    def get_queryset(self):
+        return ToDoItem.objects.filter(todo_list_id=self.kwargs["list_id"])
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context["todo_list"] = ToDoList.objects.get(id=self.kwargs["list_id"])
+        return context
+
+class ListCreate(CreateView):
+    model = ToDoList
+    fields = ["title"]
+
+    def get_context_data(self):
+        context = super(ListCreate, self).get_context_data()
+        context["title"] = "Add a new list"
+        return context
+
+class ItemCreate(CreateView):
+    model = ToDoItem
+    fields = [
+        "todo_list",
+        "title",
+        "description",
+        "due_date",
+    ]
+
+    def get_initial(self):
+        initial_data = super(ItemCreate, self).get_initial()
+        todo_list = ToDoList.objects.get(id=self.kwargs["list_id"])
+        initial_data["todo_list"] = todo_list
+        return initial_data
+
+    def get_context_data(self):
+        context = super(ItemCreate, self).get_context_data()
+        todo_list = ToDoList.objects.get(id=self.kwargs["list_id"])
+        context["todo_list"] = todo_list
+        context["title"] = "Create a new item"
+        return context
+
+    def get_success_url(self):
+        return reverse("list", args=[self.object.todo_list_id])
+
+class ItemUpdate(UpdateView):
+    model = ToDoItem
+    fields = [
+        "todo_list",
+        "title",
+        "description",
+        "due_date",
+    ]
+
+    def get_context_data(self):
+        context = super(ItemUpdate, self).get_context_data()
+        context["todo_list"] = self.object.todo_list
+        context["title"] = "Edit item"
+        return context
+
+    def get_success_url(self):
+        return reverse("list", args=[self.object.todo_list_id])
+    
+    # todo_list/todo_app/views.py
+class ListDelete(DeleteView):
+    model = ToDoList
+    
+    success_url = reverse_lazy("index")
+
+class ItemDelete(DeleteView):
+    model = ToDoItem
+
+    def get_success_url(self):
+        return reverse_lazy("list", args=[self.kwargs["list_id"]])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["todo_list"] = self.object.todo_list
+        return context
+    
+class ListUpdate(UpdateView):
+    model = ToDoList
+    fields = ["title"]
+    success_url=reverse_lazy("index")
+    def get_context_data(self):
+        context=super(ListUpdate,self).get_context_data()
+        context["title"]="Edit list"
+        return context
+
+class CheckItem(View):
+    def post(self, request, list_id, pk):
+        
+        item = get_object_or_404(ToDoItem,pk=pk)
+        item.completed = not item.completed
+        item.save()
+        return redirect("list", list_id)
+    
+    
